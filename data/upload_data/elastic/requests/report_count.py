74fbe3371d6b44b1f75e4855fcdf229fe7aefd36
FaezehAD <FaezehAD2001@gmail.com>
2023-12-30 09:09:45 +0330

    clean up workspace

--- data/upload_data/elastic/requests/report_count.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import requests
-
-with open("./../data/config_variables/BASE_URL.pkl", "rb") as f:
-    BASE_URL = pickle.load(f)
-
-response = requests.get(BASE_URL + "reports/_count", timeout=200)
-print(response.text)
